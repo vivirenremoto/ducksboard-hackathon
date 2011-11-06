82c47b7fbbf062822a821d834d96d072b5be514d
Miquel Camps Orteza <gafeman@gmail.com>
2011-11-06 18:26:05 +0100

    webcam fix

--- stats_webcam.php
+++ stats_webcam.php
@@ -3,7 +3,7 @@
 require 'config.php';
 
 // new york
-$img = 'http://images.earthcam.com/ec_metros/ourcams/lennon.jpg?v=' . time();
+$img = 'http://207.251.86.248/cctv20.jpg?v=' . time();
 $ch = curl_init('https://push.ducksboard.com/values/12556/');
 curl_setopt($ch, CURLOPT_USERPWD, API_KEY . ":ignored"); 
 curl_setopt ($ch, CURLOPT_POSTFIELDS, "{\"value\": {\"source\": \"$img\"} }");
@@ -27,7 +27,7 @@ curl_setopt ($ch, CURLOPT_POST, 1);
 curl_exec ($ch);
 
 // tokyo
-$img = 'http://www.playawebcams.com/webcams/imagenes/webcam-tokyo.jpg?v=' . time();
+$img = 'http://221.255.120.171/axis-cgi/jpg/image.cgi?v=' . time();
 $ch = curl_init('https://push.ducksboard.com/values/12559/');
 curl_setopt($ch, CURLOPT_USERPWD, API_KEY . ":ignored"); 
 curl_setopt ($ch, CURLOPT_POSTFIELDS, "{\"value\": {\"source\": \"$img\"} }");
